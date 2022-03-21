9a32284d110214a753b7201d6b4402627395dc54
Taner Özer <tanerozer16@gmail.com>
2022-03-21 17:35:48 +0300

    post blogs middlewares

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 const express = require("express");
-
+const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
 const ejs = require('ejs');
+const Blog = require('./models/Blog');
+
+// connect db 
+mongoose.connect('mongodb://localhost/blogPost-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 // Template Engine
 app.set("view engine", "ejs");
 
+// Middlewares
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static("public"))
+
 // routes 
-app.get('/', (req, res) => {
-    res.render("index")
+app.get('/', async (req, res) => {
+    const blogs = await Blog.find({});
+    res.render("index",{blogs});
 });
 app.get('/about', (req, res) => {
     res.render("about")
 });
 app.get('/add_post', (req, res) => {
-    res.render("post")
+    res.render("add_post")
 });
 
-app.use(express.static("public"))
+
+// Post
+app.post('/blogs', async (req, res) => { // async - await
+    await Blog.create(req.body)
+    // console.log(req.body);
+    res.redirect('/')
+});
+
+
 const port = 3030;
 app.listen(port, () => {
     console.log(`Sunucu ${port} portunda başlatildi..`)

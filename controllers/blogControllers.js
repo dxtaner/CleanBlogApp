097ce6f81fa1a6cbccd41de35eec953e6300302c
Taner Özer <tanerozer16@gmail.com>
2022-03-26 17:47:25 +0300

    blogControl

---
+++ controllers/blogControllers.js
@@ -0,0 +1,33 @@
+const Blog = require("../models/Blog")
+
+exports.getAllBlogs = async (req, res) => {
+    const blogs = await Blog.find({}).sort('-dateCreated');
+    res.render("index", { blogs });
+}
+
+exports.getBlog = async (req, res) => {
+    const blog = await Blog.findById(req.params.id);
+    res.render("blog", { blog })
+}
+
+exports.createBlog = async (req, res) => { // async - await
+    await Blog.create({
+        ...req.body,
+    })
+    res.redirect("/")
+}
+
+exports.updateBlog = async (req, res) => {
+    const blog = await Blog.findOne({ _id: req.params.id });
+    blog.title = req.body.title
+    blog.detail = req.body.detail
+    blog.save()
+
+    res.redirect(`/blogs/${req.params.id}`)
+}
+
+exports.deleteBlog = async (req, res) => {
+    const blog = await Blog.findOne({ _id: req.params.id });
+    await Blog.findByIdAndRemove(req.params.id);
+    res.redirect('/');
+}
